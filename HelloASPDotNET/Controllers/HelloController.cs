cd6cc442eec860b564f3953e3021ef74558e7697
Ashley <ashley.horn15@yahoo.com>
2023-03-23 11:03:31 -0500

    HelloASPDotNET Razor Template Save

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string html = "<h1>" + "Hello World!" + "</h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
